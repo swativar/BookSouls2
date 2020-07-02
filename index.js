a0c38a88bacef20b89500ed7c51f9e7d484df9d1
theunknown31 <32437156+theunknown31@users.noreply.github.com>
2020-07-02 11:24:53 +0530

    s

--- index.js
+++ index.js
@@ -27,3 +27,34 @@ buttons.forEach(function(button){
     }
     }
 });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slider");
+    console.log(slides);
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+// function myFunction(x) {
+//     if (x.matches) { // If media query matches
+//         $('.fade_e').removeClass('slider');
+//         console.log($('.fade_e').attr("style"));
+//     } else {
+//         $('.fade_e').addClass('slider');
+//         showSlides(slideIndex);
+//     }
+//   }
+  
+  //var x = window.matchMedia("(max-width: 600px)");
+ // myFunction(x) // Call listener function at run time
