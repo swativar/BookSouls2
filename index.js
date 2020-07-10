37ed2de1164adeb30f0e626dc0050aeffb4b2a20
theunknown31 <32437156+theunknown31@users.noreply.github.com>
2020-07-10 17:19:06 +0530

    h

--- index.js
+++ index.js
@@ -1,49 +1,15 @@
 // automatic slideshow off //
-$(document).ready(function () {
-    $("#demo").carousel({
-        interval: false
-    });
+$(document).ready(function(){
+	$("#demo").carousel({
+		interval : false
+	});
 });
-$(document).ready(function () {
-    $("#demo1").carousel({
-        interval: false
-    });
-    $("#demo2").carousel({
-        interval: false
-    });
-    $("#art").carousel({
-        interval: false
-    });
-    $("#art1").carousel({
-        interval: false
-    });
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 0) {
-            $('.navbar').addClass('shadow');
-        } else {
-            $('.navbar').removeClass('shadow');
-        }
-    });
-    $("[data-trigger]").on("click", function () {
-        var trigger_id = $(this).attr('data-trigger');
-        $(trigger_id).toggleClass("show");
-        $('body').toggleClass("offcanvas-active");
-    });
-
-    // close button 
-    $(".btn-close").click(function (e) {
-        $(".navbar-collapse").removeClass("show");
-        $("body").removeClass("offcanvas-active");
-    });
+$(document).ready(function(){
+	$("#demo2").carousel({
+		interval : false
+	});
 });
 
-var docWidth = document.documentElement.offsetWidth;
-
-[].forEach.call(
-    document.querySelectorAll('*'),
-    function (el) {
-        if (el.offsetWidth > docWidth) {
-            console.log(el);
-        }
-    }
-);
+if(screen.width<=699){
+    document.location = "mobile.html";
+}
---
+++ mobile.js
@@ -0,0 +1,53 @@
+// automatic slideshow off //
+$(document).ready(function () {
+    $("#demo").carousel({
+        interval: false
+    });
+});
+$(document).ready(function () {
+    $("#demo1").carousel({
+        interval: false
+    });
+    $("#demo2").carousel({
+        interval: false
+    });
+    $("#art").carousel({
+        interval: false
+    });
+    $("#art1").carousel({
+        interval: false
+    });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.navbar').addClass('shadow');
+        } else {
+            $('.navbar').removeClass('shadow');
+        }
+    });
+    $("[data-trigger]").on("click", function () {
+        var trigger_id = $(this).attr('data-trigger');
+        $(trigger_id).toggleClass("show");
+        $('body').toggleClass("offcanvas-active");
+    });
+
+    // close button 
+    $(".btn-close").click(function (e) {
+        $(".navbar-collapse").removeClass("show");
+        $("body").removeClass("offcanvas-active");
+    });
+});
+
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+    document.querySelectorAll('*'),
+    function (el) {
+        if (el.offsetWidth > docWidth) {
+            console.log(el);
+        }
+    }
+);
+
+if(screen.width>700){
+    document.location = "index.html";
+}
