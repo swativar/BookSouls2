f0c5be5ff73bda12878351306bfc7e016dffc7e8
theunknown31 <32437156+theunknown31@users.noreply.github.com>
2020-07-13 18:48:16 +0530

    h

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-// automatic slideshow off //
 $(document).ready(function () {
     $("#demo").carousel({
         interval: false
     });
-});
-$(document).ready(function () {
     $("#demo1").carousel({
         interval: false
     });
     $("#demo2").carousel({
-        interval: false
-    });
-    $("#art").carousel({
-        interval: false
-    });
-    $("#art1").carousel({
         interval: false
     });
     $(window).scroll(function () {
@@ -24,20 +15,28 @@ $(document).ready(function () {
             $('.navbar').removeClass('shadow');
         }
     });
-    $("[data-trigger]").on("click", function () {
-        var trigger_id = $(this).attr('data-trigger');
-        $(trigger_id).toggleClass("show");
-        $('body').toggleClass("offcanvas-active");
-    });
-
-    // close button 
-    $(".btn-close").click(function (e) {
-        $(".navbar-collapse").removeClass("show");
-        $("body").removeClass("offcanvas-active");
-    });
 });
 
-var docWidth = document.documentElement.offsetWidth;
+(() => {
+    'use strict';
+    // Page is loaded
+    const objects = document.getElementsByClassName('images');
+    console.log(objects);
+    Array.from(objects).map((item) => {
+      // Start loading image
+      const img = new Image();
+      img.src = item.dataset.src;
+      // Once image is loaded replace the src of the HTML element
+      img.onload = () => {
+        item.classList.remove('asyncImage');
+        return item.nodeName === 'IMG' ? 
+          item.src = item.dataset.src :        
+          item.style.backgroundImage = `url(${item.dataset.src})`;
+      };
+    });
+  })();
+
+  var docWidth = document.documentElement.offsetWidth;
 
 [].forEach.call(
     document.querySelectorAll('*'),
@@ -48,3 +47,51 @@ var docWidth = document.documentElement.offsetWidth;
     }
 );
 
+var links = document.querySelectorAll('.js_inspire_link');
+links.forEach(function(link){
+    link.onclick = function(e){
+        var length = links.length;
+        for (var i=0; i<length; i++){
+            links[i].children[0].classList.remove('active');
+        }
+        console.log(link.children);
+        var li = link.children[0];
+        console.log(li);
+        li.classList.add("active");
+    }
+});
+
+var pod_links = document.querySelectorAll('.js_pod_link');
+pod_links.forEach(function(link){
+    link.onclick = function(e){
+        var length = links.length;
+        for (var i=0; i<length; i++){
+            pod_links[i].children[0].classList.remove('active');
+        }
+        console.log(link.children);
+        var li = link.children[0];
+        console.log(li);
+        li.classList.add("active");
+    }
+});
+
+var items = document.querySelectorAll('.item');
+var first_item = items[0];
+var last_item = items[items.length-1];
+var first_clone = first_item.cloneNode('true');
+var last_clone = last_item.cloneNode('true');
+console.log(first_clone,last_clone);
+var last_slide = document.querySelector('#demoe');
+var first_slide = document.querySelector('#demoa');
+last_slide.appendChild(first_clone);
+first_slide.prepend(last_clone);
+
+var podcast = document.querySelectorAll('.js_pod');
+var first_pod = podcast[0];
+var last_pod = podcast[podcast.length-1];
+var first_pod_clone = first_pod.cloneNode('true');
+var last_pod_clone = last_pod.cloneNode('true');
+var last_pod_slide = document.querySelector('#podcast_in5');
+var first_pod_slide = document.querySelector('#podcast_in1');
+last_pod_slide.appendChild(first_pod_clone);
+first_pod_slide.prepend(last_pod_clone);
