517c0639ea7cca31ddcf6b9611d0897645099b28
theunknown31 <32437156+theunknown31@users.noreply.github.com>
2020-07-22 13:37:33 +0530

    l

--- index.js
+++ index.js
@@ -1,93 +1,33 @@
 $(document).ready(function () {
-    $("#demo").carousel({
+    $("#article").carousel({
         interval: false
     });
-    $("#demo1").carousel({
+    $("#quick").carousel({
         interval: false
     });
-    $("#demo2").carousel({
+    $("#publisher").carousel({
         interval: false
     });
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 0) {
-            $('.navbar').addClass('shadow');
-        } else {
-            $('.navbar').removeClass('shadow');
-        }
-    });
-});
 
-(() => {
-    'use strict';
-    // Page is loaded
-    const objects = document.getElementsByClassName('images');
-    console.log(objects);
-    Array.from(objects).map((item) => {
-      // Start loading image
-      const img = new Image();
-      img.src = item.dataset.src;
-      // Once image is loaded replace the src of the HTML element
-      img.onload = () => {
-        item.classList.remove('asyncImage');
-        return item.nodeName === 'IMG' ? 
-          item.src = item.dataset.src :        
-          item.style.backgroundImage = `url(${item.dataset.src})`;
-      };
-    });
-  })();
+})
 
-  var docWidth = document.documentElement.offsetWidth;
+var color = window.getComputedStyle(
+	document.querySelector('header'), ':before'
+).getPropertyValue('right');
+console.log(color);
 
-[].forEach.call(
-    document.querySelectorAll('*'),
-    function (el) {
-        if (el.offsetWidth > docWidth) {
-            console.log(el);
-        }
-    }
-);
+header = document.querySelector('header');
 
-var links = document.querySelectorAll('.js_inspire_link');
-links.forEach(function(link){
-    link.onclick = function(e){
-        var length = links.length;
-        for (var i=0; i<length; i++){
-            links[i].children[0].classList.remove('active');
-        }
-        console.log(link.children);
-        var li = link.children[0];
-        console.log(li);
-        li.classList.add("active");
-    }
-});
-
-var pod_links = document.querySelectorAll('.js_pod_link');
-pod_links.forEach(function(link){
-    link.onclick = function(e){
-        var length = pod_links.length;
-        for (var i=0; i<length; i++){
-            pod_links[i].children[0].classList.remove('active');
-        }
-        console.log(link.children);
-        var li = link.children[0];
-        console.log(li);
-        li.classList.add("active");
-    }
-});
-
-
-// The function actually applying the offset
-function offsetAnchor() {
-    if(location.hash.length !== 0) {
-        window.scrollTo(window.scrollX, window.scrollY - 100);
-    }
+function scroll(){
+    var article = document.querySelector('#scroll_article')
+    var top = article.getBoundingClientRect().top;
+    window.scrollBy(0,top);
+    /*$('body,html').animate({
+        scrollTop: top
+      }, 800,
+    
+      );*/
 }
 
-// This will capture hash changes while on the page
-window.addEventListener("hashchange", offsetAnchor);
-
-// This is here so that when you enter the page with a hash,
-// it can provide the offset in that case too. Having a timeout
-// seems necessary to allow the browser to jump to the anchor first.
-window.setTimeout(offsetAnchor, 1);
+setTimeout(scroll,7000);
 
