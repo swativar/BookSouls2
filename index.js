8a2826adcf4915694746e6cb7111010b2f372767
theunknown31 <32437156+theunknown31@users.noreply.github.com>
2020-07-23 15:47:57 +0530

    q

--- index.js
+++ index.js
@@ -33,11 +33,11 @@ function Pageloadtime() {
 document.onreadystatechange = function() { 
     if (document.readyState !== "complete") { 
         document.querySelector("body").style.visibility = "hidden"; 
-        document.querySelector("#loader").style.visibility = "visible"; 
+        document.querySelector("#loader").style.visibility = "visible";
+        document.documentElement.style.overflow = 'hidden';
     } else { 
         document.querySelector("#loader").style.display = "none"; 
-        document.querySelector("body").style.visibility = "visible"; 
+        document.querySelector("body").style.visibility = "visible";
+        document.documentElement.style.overflow = 'auto'; 
     } 
 };
-
-
