39d23fd43fe582f42c67e6db666a759a7e25ac4a
theunknown31 <32437156+theunknown31@users.noreply.github.com>
2020-07-23 12:05:29 +0530

    h

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-var color = window.getComputedStyle(
-	document.querySelector('header'), ':before'
-).getPropertyValue('right');
-console.log(color);
 
 var header_explore = document.querySelector('#header_explore');
 
@@ -9,10 +5,39 @@ header_explore.onclick = function scroll(){
     var article = document.querySelector('#scroll_article')
     var top = article.getBoundingClientRect().top-50;
     window.scrollBy(0,top);
-    /*$('body,html').animate({
-        scrollTop: top
-      }, 800,
-    
-      );*/
 }
+
+/*document.body.classList.add('js-loading');
+window.addEventListener('load', showAnimation());
+
+function showAnimation(){
+    document.body.classList.remove('js-loading');
+}*/
+
+var before_loadtime = new Date().getTime();  
+window.onload = Pageloadtime;  
+function Pageloadtime() {  
+    var aftr_loadtime = new Date().getTime();  
+    // Time calculating in seconds  
+    pgloadtime = (aftr_loadtime - before_loadtime) / 1000;
+    console.log(pgloadtime);
+    var navbar_brand = document.querySelector('.navbar-brand');
+    var header_illus1 = document.querySelector('.header_illustration img:first-child');
+    var header_illus2 = document.querySelector('.header_illustration img:last-child');
+    var header_content = document.querySelector('.header_content');
+    navbar_brand.style.animationDelay = pgloadtime+'s';
+    header_illus1.style.animationDelay = pgloadtime+'s';
+    header_illus2.style.animationDelay = pgloadtime+'s';
+    header_content.style.animationDelay = pgloadtime+'s';
+}
+document.onreadystatechange = function() { 
+    if (document.readyState !== "complete") { 
+        document.querySelector("body").style.visibility = "hidden"; 
+        document.querySelector("#loader").style.visibility = "visible"; 
+    } else { 
+        document.querySelector("#loader").style.display = "none"; 
+        document.querySelector("body").style.visibility = "visible"; 
+    } 
+};
+
 
