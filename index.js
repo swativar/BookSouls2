98e69187e398fda0aff1cf828f51b7393c692b46
swativar <64944666+swativar@users.noreply.github.com>
2020-08-14 12:13:40 +0530

    a

--- index.js
+++ index.js
@@ -51,3 +51,28 @@ function searchPage(){
 function allPage(){
     search.style.display = "none";
 }
+
+
+//
+$("[data-trigger]").on("click", function () {
+    var trigger_id = $(this).attr('data-trigger');
+    $(trigger_id).toggleClass("show");
+    $('body').toggleClass("offcanvas-active");
+});
+
+// close button 
+$(".btn-close").click(function (e) {
+    $(".navbar-collapse").removeClass("show");
+    $("body").removeClass("offcanvas-active");
+});
+
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+    document.querySelectorAll('*'),
+    function (el) {
+        if (el.offsetWidth > docWidth) {
+            console.log(el);
+        }
+    }
+);
