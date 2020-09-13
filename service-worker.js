57884702d15d1e4eb6b266ad03db20b25eab26ac
swativar <64944666+swativar@users.noreply.github.com>
2020-09-13 10:55:35 +0530

    i

--- service-worker.js
+++ service-worker.js
@@ -19,6 +19,17 @@ if (workbox) {
     { url: '/common.css', revision: null },
     { url: "/assets-Error-Page/warning.gif", revision: null },
     { url: "/assets-Error-Page/PointerRotated.png", revision: null },
+    { url: "/assets/blur.png", revision: null },
+    { url: "/assets/footer_bg2.png", revision: null},
+    { url: "/assets/footer-illustration-cropped.png", revision: null},
+    { url: "/Favicons/96_96.png", revision: null},
+    { url: "/Favicons/72_72.png", revision: null},
+    { url: "/Favicons/128_128.png", revision: null},
+    { url: "/Favicons/144_144.png", revision: null},
+    { url: "/Favicons/152_152.png", revision: null},
+    { url: "/Favicons/192_192.png", revision: null},
+    { url: "/Favicons/384_384.png", revision: null},
+    { url: "/Favicons/384_384.png", revision: null},
   ]);
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
@@ -28,15 +39,19 @@ if (workbox) {
 //   e.waitUntil(self.skipWaiting());
 // });
 
-// self.addEventListener('activate', e => {
-//   self.clients.claim();
-// });
-
+const suffix = 'v1';
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => keys.filter(key => !key.endsWith(suffix)))
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+  );
+});
 
 workbox.routing.registerRoute(
   ({ request }) => request.destination === 'style',
-  new workbox.strategies.CacheFirst({
-    cacheName: 'static-css',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'static-css-v1',
     plugins: [
       new workbox.expiration.ExpirationPlugin({
         maxEntries: 200,
@@ -50,11 +65,26 @@ workbox.routing.registerRoute(
 
 workbox.routing.registerRoute(
   ({ request }) => request.destination === 'script',
-  new workbox.strategies.CacheFirst({
-    cacheName: 'static-js',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'static-js-v1',
     plugins: [
       new workbox.expiration.ExpirationPlugin({
         maxEntries: 200,
+      }),
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [0, 200]
+      })
+    ]
+  })
+);
+
+workbox.routing.registerRoute(
+  ({url}) => url.pathname.endsWith('.svg'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'static-svg-v1',
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 50,
       }),
       new workbox.cacheableResponse.CacheableResponsePlugin({
         statuses: [0, 200]
@@ -65,14 +95,14 @@ workbox.routing.registerRoute(
 
 workbox.routing.registerRoute(
   ({ request }) => request.destination === 'image',
-  new workbox.strategies.CacheFirst({
+  new workbox.strategies.StaleWhileRevalidate({
     // Use a custom cache name.
-    cacheName: 'static-images',
+    cacheName: 'static-images-v1',
     plugins: [
       new workbox.expiration.ExpirationPlugin({
         maxEntries: 250,
         purgeOnQuotaError: true,
-        maxAgeSeconds: 30 * 24 * 60 * 60,
+        maxAgeSeconds: 60 * 60,
       }),
       new workbox.cacheableResponse.CacheableResponsePlugin({
         statuses: [0, 200]
@@ -82,14 +112,9 @@ workbox.routing.registerRoute(
 );
 
 workbox.routing.registerRoute(
-  ({ event, url }) => event.request.destination === 'document' && (url.pathname === '/index.html' || url.pathname === '/'),
+  ({ url }) => url.origin === location.origin && (url.pathname === '/index.html' || url.pathname === '/'),
   new workbox.strategies.CacheFirst({
-    cacheName: 'static-htmls',
     plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 200,
-        maxAgeSeconds: 30 * 24 * 60 * 60,
-      }),
       new workbox.cacheableResponse.CacheableResponsePlugin({
         statuses: [0, 200]
       })
@@ -98,9 +123,14 @@ workbox.routing.registerRoute(
 );
 
 workbox.routing.registerRoute(
+  ({ event, url }) => event.request.destination === 'document' && (url.pathname !== '/index.html' || url.pathname !== '/'),
+  new workbox.strategies.NetworkOnly()
+);
+
+workbox.routing.registerRoute(
   ({ event }) => event.request.destination === 'font',
   new workbox.strategies.CacheFirst({
-    cacheName: 'static-fonts',
+    cacheName: 'static-fonts-v1',
     plugins: [
       new workbox.expiration.ExpirationPlugin({
         maxEntries: 60,
@@ -139,7 +169,9 @@ workbox.routing.setCatchHandler(({ event }) => {
     case 'document':
       return workbox.precaching.matchPrecache('/offline.html');
       break;
-
+    case 'image':
+      return workbox.precaching.matchPrecache('/assets/blur.png');
+      break;
     default:
       // If we don't have a fallback, just return an error response.
       return Response.error();
