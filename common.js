16b49f1ca6441d30f28effc086286b3842632afb
swativar <64944666+swativar@users.noreply.github.com>
2020-10-02 20:33:00 +0530

    i

--- common.js
+++ common.js
@@ -142,3 +142,21 @@ function searchResult() {
         window.location = "searchresult.html";
     }
 }
+
+(() => {
+    'use strict';
+    // Page is loaded
+    const objects = document.getElementsByClassName('lazy');
+    Array.from(objects).map((item) => {
+        // Start loading image
+        const img = new Image();
+        img.src = item.dataset.src;
+        // Once image is loaded replace the src of the HTML element
+        img.onload = () => {
+            item.classList.remove('lazy');
+            return item.nodeName === 'IMG' ?
+                item.src = item.dataset.src :
+                item.style.backgroundImage = `url(${item.dataset.src})`;
+        };
+    });
+})();
